6c5e74798a8f77050af4e8014089060010f5412a
samyhaff <haffoudhisamy@gmail.com>
2020-12-29 22:30:49 +0100

    undo fakefullscreen

--- dwm.c
+++ dwm.c
@@ -541,7 +541,7 @@ clientmessage(XEvent *e)
 		if (cme->data.l[1] == netatom[NetWMFullscreen]
 		|| cme->data.l[2] == netatom[NetWMFullscreen])
 			setfullscreen(c, (cme->data.l[0] == 1 /* _NET_WM_STATE_ADD    */
-				|| cme->data.l[0] == 2 /* _NET_WM_STATE_TOGGLE */));
+ 				|| (cme->data.l[0] == 2 /* _NET_WM_STATE_TOGGLE */ && !c->isfullscreen)));
 	} else if (cme->message_type == netatom[NetActiveWindow]) {
 		if (c != selmon->sel && !c->isurgent)
 			seturgent(c, 1);
@@ -571,6 +571,7 @@ void
 configurenotify(XEvent *e)
 {
 	Monitor *m;
+    Client *c;
 	XConfigureEvent *ev = &e->xconfigure;
 	int dirty;
 
@@ -583,6 +584,9 @@ configurenotify(XEvent *e)
 			drw_resize(drw, sw, bh);
 			updatebars();
 			for (m = mons; m; m = m->next) {
+ 				for (c = m->clients; c; c = c->next)
+ 					if (c->isfullscreen)
+ 						resizeclient(c, m->mx, m->my, m->mw, m->mh);
 				XMoveResizeWindow(dpy, m->barwin, m->wx, m->by, m->ww, bh);
 			}
 			focus(NULL);
@@ -1175,6 +1179,8 @@ movemouse(const Arg *arg)
 
 	if (!(c = selmon->sel))
 		return;
+ 	if (c->isfullscreen) /* no support moving fullscreen windows by mouse */
+ 		return;
 	restack(selmon);
 	ocx = c->x;
 	ocy = c->y;
@@ -1328,6 +1334,8 @@ resizemouse(const Arg *arg)
 
 	if (!(c = selmon->sel))
 		return;
+ 	if (c->isfullscreen) /* no support resizing fullscreen windows by mouse */
+ 		return;
 	restack(selmon);
 	ocx = c->x;
 	ocy = c->y;
@@ -1581,10 +1589,24 @@ setfullscreen(Client *c, int fullscreen)
 		XChangeProperty(dpy, c->win, netatom[NetWMState], XA_ATOM, 32,
 			PropModeReplace, (unsigned char*)&netatom[NetWMFullscreen], 1);
 		c->isfullscreen = 1;
+ 		c->oldstate = c->isfloating;
+ 		c->oldbw = c->bw;
+ 		c->bw = 0;
+ 		c->isfloating = 1;
+ 		resizeclient(c, c->mon->mx, c->mon->my, c->mon->mw, c->mon->mh);
+ 		XRaiseWindow(dpy, c->win);
 	} else if (!fullscreen && c->isfullscreen){
 		XChangeProperty(dpy, c->win, netatom[NetWMState], XA_ATOM, 32,
 			PropModeReplace, (unsigned char*)0, 0);
 		c->isfullscreen = 0;
+ 		c->isfloating = c->oldstate;
+ 		c->bw = c->oldbw;
+ 		c->x = c->oldx;
+ 		c->y = c->oldy;
+ 		c->w = c->oldw;
+ 		c->h = c->oldh;
+ 		resizeclient(c, c->x, c->y, c->w, c->h);
+ 		arrange(c->mon);
 	}
 }
 
@@ -1719,7 +1741,7 @@ showhide(Client *c)
 	if (ISVISIBLE(c)) {
 		/* show clients top down */
 		XMoveWindow(dpy, c->win, c->x, c->y);
-		if (!c->mon->lt[c->mon->sellt]->arrange || c->isfloating)
+ 		if ((!c->mon->lt[c->mon->sellt]->arrange || c->isfloating) && !c->isfullscreen)
 			resize(c, c->x, c->y, c->w, c->h, 0);
 		showhide(c->snext);
 	} else {
@@ -1768,6 +1790,8 @@ tagmon(const Arg *arg)
 {
 	if (!selmon->sel || !mons->next)
 		return;
+ 	if (selmon->sel->isfullscreen) /* no support for fullscreen windows */
+ 		return;
 	sendmon(selmon->sel, dirtomon(arg->i));
 }
 
