27c815f43db733105acdfc78a3acb5e044d0c0c1
samyhaff <haffoudhisamy@gmail.com>
2020-12-30 07:42:39 +0100

    changed design

--- config.def.h
+++ config.def.h
@@ -8,12 +8,12 @@ static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "iosevka:size=10", "fontawesome:size=14" };
+static const char *fonts[]          = { "iosevka:size=11" }
 static const char dmenufont[]       = "iosevka:size=10.5";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
+static const char col_gray4[]       = "#282828";
 static const char col_cyan[]        = "#ebdbb2";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
