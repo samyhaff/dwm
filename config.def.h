f45d208f09903e6cb6dceeb3f9b0737da117cc89
samyhaff <haffoudhisamy@gmail.com>
2021-01-04 22:20:00 +0100

    save before adding 2 new patches

--- config.def.h
+++ config.def.h
@@ -7,13 +7,13 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "iosevka:size=10", };
+static const char *fonts[]          = { "iosevka:size=10", "FontAwesome:size=10", };
 static const char dmenufont[]       = "iosevka:size=10.5";
 static const char col_gray1[]       = "#282828";
 static const char col_gray2[]       = "#282828";
 static const char col_gray3[]       = "#ebdbb2";
 static const char col_gray4[]       = "#ebdbb2";
-static const char col_cyan[]        = "#458588";
+static const char col_cyan[]        = "#9d0006";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -30,6 +30,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "qutebrowser",  NULL,       NULL,       1 << 1,       0,           -1 },
 	{ "discord",  NULL,       NULL,       1 << 2,       0,           -1 },
 	{ "zoom",     NULL,       NULL,       1 << 3,       0,           -1 },
 };
@@ -69,8 +70,7 @@ static const char *audiomute[] = {"pactl", "set-sink-mute", "@DEFAULT_SINK@", "t
 static const char *micmute[] = {"pactl", "set-source-mute", "@DEFAULT_SOURCE@", "toggle", NULL};
 static const char *fullscreenshot[] = {"fullscreenshot", NULL};
 static const char *windowscreenshot[] = {"windowscreenshot", NULL}; 
-static const char *emacs[] = {"emacs", NULL};
-static const char *qutebrowser[] = {"qutebrowser", NULL};
+static const char *firefox[] = {"firefox", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -82,8 +82,7 @@ static Key keys[] = {
     { 0, XF86XK_AudioLowerVolume, spawn, {.v = volumedown } },
     { 0, XF86XK_MonBrightnessUp, spawn, {.v = brightnessup } },
     { 0, XF86XK_MonBrightnessDown, spawn, {.v = brightnessdown } },
-	{ MODKEY,                       XK_e,      spawn,          {.v = emacs } },
-	{ MODKEY,                       XK_n,      spawn,          {.v = qutebrowser } },
+	{ MODKEY,                       XK_n,      spawn,          {.v = firefox } },
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
